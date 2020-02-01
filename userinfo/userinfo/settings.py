d34c579c491db6526fa3fb1914573b267f9fe6c9
shivamrawat829 <shivamrawat829@gmail.com>
2020-02-01 11:53:09 +0530

    d

--- userinfo/userinfo/settings.py
+++ userinfo/userinfo/settings.py
@@ -94,12 +94,24 @@ WSGI_APPLICATION = 'userinfo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myTechviourDB',
+        'USER':'shivamrawat829',
+        'PASSWORD':'legend829',
+        'HOST':'mytechviourdb.cfnpm4aojkvd.ap-south-1.rds.amazonaws.com',
+        'PORT':'5432',
     }
 }
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
